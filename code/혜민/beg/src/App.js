131a1e57c2c0f747aed6d84166336fcd5f637470
윤혜민 <h2m0@swu.ac.kr>
2022-05-08 23:29:31 +0900

    [EDIT] 1-04. What is jsx?

--- code/혜민/beg/src/App.js
+++ code/혜민/beg/src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import Hello from './Hello';
 
 function App() {
+  var 현재상태 = 'refund';
   return (
-    <div>
-      <Hello />
-    </div>
+    <>
+      <div>
+        <Hello />
+      </div>
+      {/* enum 실습 */}
+      <div>
+        {
+          {
+            info: <p>상품정보</p>,
+            shipping: <p>배송관련</p>,
+            refund: <p>환불약관</p>,
+          }[현재상태]
+        }
+      </div>
+    </>
   );
 }
 
